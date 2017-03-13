162d2a5fa9ddc5a4d5a0e0615f61229739b05f91
Eliot Blennerhassett <eliot@blennerhassett.gen.nz>
2017-03-13 14:22:03 +1300

    keycodes for Magic Lighting Remote Controller

---
+++ src/magic_remote.h
@@ -0,0 +1,36 @@
+/** keycodes from magic lighting 4 x 6 key remote */
+enum {
+ MAGIC_plus = 0x00ffa05f,
+ MAGIC_minus = 0x00ff20df,
+ MAGIC_off = 0x00ff609f,
+ MAGIC_on = 0x00ffe01f,
+
+ MAGIC_R = 0x00ff906f,
+ MAGIC_G = 0x00ff10ef,
+ MAGIC_B = 0x00ff50af,
+ MAGIC_W = 0x00ffd02f,
+
+// 3 rows of intermediate colour buttons
+ MAGIC_r1 = 0x00ffb04f,
+ MAGIC_g1 = 0x00ff30cf,
+ MAGIC_b1 = 0x00ff708f,
+ MAGIC_flash = 0x00fff00f,
+
+ MAGIC_r2 = 0x00ffa857,
+ MAGIC_g2 = 0x00ff28d7,
+ MAGIC_b2 = 0x00ff6897,
+ MAGIC_strobe = 0x00ffe817,
+
+ MAGIC_r3 = 0x00ff9867,
+ MAGIC_g3 = 0x00ff18e7,
+ MAGIC_b3 = 0x00ff58a7,
+ MAGIC_fade = 0x00ffd827,
+
+
+ MAGIC_Y = 0x00ff8877,
+ MAGIC_C = 0x00ff08f7,
+ MAGIC_M = 0x00ff48b7,
+ MAGIC_smooth = 0x00ffc837,
+
+ MAGIC_release = 0xffffffff // release or repeat
+};
