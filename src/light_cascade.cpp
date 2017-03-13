01e1d44b1af9baaa3784294a6c8b7f7532ba2821
Eliot Blennerhassett <eliot@blennerhassett.gen.nz>
2017-03-13 14:20:57 +1300

    Convert timer back to struct

--- src/light_cascade.cpp
+++ src/light_cascade.cpp
@@ -10,6 +10,8 @@
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
+/********** Keymapping *********/
+
 static unsigned long last_press = 0;
 static unsigned long keycode_down = 0;
 
@@ -28,12 +30,7 @@ static unsigned long map_key(unsigned long keycode)
 	return 0;
 }
 
-
-struct Lights {
-    const uint8_t *pins;
-    int num_pins;
-    int current_idx;
-};
+/********** Settings object in EEPROM *********/
 
 struct Settings {
     int magic;
@@ -76,6 +73,12 @@ static void eeprom_init(void)
 	eeprom_update();
 }
 
+/********** Lights object *********/
+struct Lights {
+    const uint8_t *pins;
+    int num_pins;
+    int current_idx;
+};
 
 static const uint8_t pins[] = {13, 12, 11, 10, 9, 8, 7, 6};
 struct Lights lights = {pins, ARRAY_SIZE(pins), 0};
@@ -119,29 +122,37 @@ static void increment_pin()
     lights.current_idx = (lights.current_idx + 1) % lights.num_pins;
 }
 
-
-class Timer {
+/********** Timer object **********/
+struct Timer {
     unsigned long start_millis;
     unsigned long delay;
-public:
-    Timer() {delay = 0;};
-    void start(unsigned int delay_ms)
-    {
-        start_millis = millis();
-        delay = delay_ms;
-        //Debug.print("Timer start ");
-        //Debug.println(delay);
-    };
-    bool expired()
-    {
-        // Debug.print(millis());
-        // Debug.print(" - ");
-        // Debug.println(start_millis);
-        return (delay == 0) || ((millis() - start_millis) >= delay);
-    };
+};
+
+void timer_init(Timer& t)
+{
+    t.delay = 0;
+}
+
+void timer_start(Timer& t, unsigned int delay_ms)
+{
+    t.start_millis = millis();
+    t.delay = delay_ms;
+    //Debug.print("Timer start ");
+    //Debug.println(delay);
+};
+
+bool timer_expired(Timer &t)
+{
+    // Debug.print(millis());
+    // Debug.print(" - ");
+    // Debug.println(start_millis);
+    return (t.delay == 0) || ((millis() - t.start_millis) >= t.delay);
 };
 
 Timer timer;
+
+
+/********** State machine **********/
 
 static enum state {on, off} current_state;
 
@@ -152,7 +163,7 @@ static void state_machine_init()
 
 static void state_machine_run()
 {
-    if (not timer.expired())
+    if (not timer_expired(timer))
         return;
 
     if (settings.gap_time >= 0) {
@@ -162,12 +173,12 @@ static void state_machine_run()
             increment_pin();
             current_state = on;
             if (settings.gap_time)
-                timer.start(settings.gap_time);
+                timer_start(timer, settings.gap_time);
                 break;
             // else fall through
         case on:
             light_on(current_pin());
-            timer.start(settings.on_time);
+            timer_start(timer, settings.on_time);
             current_state = off;
             break;
         }
@@ -175,18 +186,20 @@ static void state_machine_run()
         switch (current_state) {
         case on:
             light_on(next_pin());
-            timer.start(-settings.gap_time);
+            timer_start(timer, -settings.gap_time);
             current_state = off;
             break;
         case off:
             light_off(current_pin());
-            timer.start(settings.on_time + settings.gap_time);
+            timer_start(timer, settings.on_time + settings.gap_time);
             increment_pin();
             current_state = on;
             break;
         }
     }
 }
+
+/********** Remote control **********/
 
 #if NEC_ONLY
 static IRReadOnlyRemote remote(IR_PIN);		// NEC
@@ -224,6 +237,8 @@ static void remote_control(void)
         }
 	}
 }
+
+/********** Interface to Arduino framework **********/
 
 void setup()
 {
